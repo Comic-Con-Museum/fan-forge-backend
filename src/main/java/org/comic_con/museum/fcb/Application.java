2f47c6272e74622516f9ed6297cd925be0f4e5e7
Nic Hartley <nxh9052@rit.edu>
2018-10-07 17:42:28 -0700

    Added S3 connection demo

--- src/main/java/org/comic_con/museum/fcb/Application.java
+++ src/main/java/org/comic_con/museum/fcb/Application.java
@@ -1,8 +1,15 @@
 package org.comic_con.museum.fcb;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,18 +22,53 @@ public class Application implements CommandLineRunner {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    @Value("${s3.access-key}")
+    String accessKey;
+
+    @Value("${s3.secret-key}")
+    String secretKey;
+
+    @Value("${s3.url}")
+    String url;
+
+    @Value("${s3.region}")
+    String region;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        LOG.info("Demoing DB connection:");
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS demo (id INTEGER PRIMARY KEY)");
-        jdbcTemplate.execute("INSERT INTO demo VALUES (1), (2), (3)");
-        jdbcTemplate.query(
-                "SELECT * FROM demo",
-                (rs, rowNum) -> String.valueOf(rs.getInt("id"))
-        ).forEach(LOG::info);
+        try {
+            LOG.info("Demoing DB connection:");
+            jdbcTemplate.execute("DROP TABLE IF EXISTS demo");
+            jdbcTemplate.execute("CREATE TABLE demo (id INTEGER PRIMARY KEY)");
+            jdbcTemplate.execute("INSERT INTO demo VALUES (1), (2), (3)");
+            jdbcTemplate.query(
+                    "SELECT * FROM demo",
+                    (rs, rowNum) -> String.valueOf(rs.getInt("id"))
+            ).forEach(LOG::info);
+            LOG.info("Done with DB stuff");
+        } catch (Exception e) {
+            LOG.error("Error occurred while doing database stuff", e);
+        }
+
+        try {
+            LOG.info("Demoing S3 connection:");
+            AWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+            AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(creds))
+                    .withEndpointConfiguration(
+                            new AwsClientBuilder.EndpointConfiguration(
+                                    url, region
+                            )
+                    )
+                    .build();
+            s3.listBuckets().forEach(b -> LOG.info(b.getName()));
+            LOG.info("Done with S3 stuff");
+        } catch (Exception e) {
+            LOG.error("Error occurred while doing S3 stuff", e);
+        }
     }
 }
