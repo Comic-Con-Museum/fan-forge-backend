fdcd615530f496a49155656e758b4c6f0ef2b2e3
Nic Hartley <nxh9052@rit.edu>
2018-10-22 22:55:46 -0400

    fixed auth; now even non-required endpoints CAN be authorized

--- src/main/java/org/comic_con/museum/fcb/config/WebSecurityConfig.java
+++ src/main/java/org/comic_con/museum/fcb/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,11 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authProvider);
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().requestMatchers(PUBLIC_URLS);
     }
 
     @Override
--- src/main/java/org/comic_con/museum/fcb/filters/BearerTokenAuthFilter.java
+++ src/main/java/org/comic_con/museum/fcb/filters/BearerTokenAuthFilter.java
@@ -2,7 +2,6 @@ package org.comic_con.museum.fcb.filters;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -17,23 +16,26 @@ import java.io.IOException;
 
 public class BearerTokenAuthFilter extends AbstractAuthenticationProcessingFilter {
     private static final Logger LOG = LoggerFactory.getLogger("filter.auth");
-
+    
     public BearerTokenAuthFilter(RequestMatcher requestMatcher) {
         super(requestMatcher);
     }
-
+    
+    @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        // every request requires an auth attempt
+        return true;
+    }
+    
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
         LOG.info("Checking auth on {} {}", req.getMethod(), req.getRequestURI());
         String header = req.getHeader("Authorization");
-        if (header == null) {
-            LOG.info("No Authorization header, can't log in");
-            throw new BadCredentialsException("No Authorization header");
-        }
-        if (!header.startsWith("Bearer ")) {
-            // ...then it's not bearer token authorization
-            LOG.info("Non-Bearer Authorization header: '{}'", header);
-            throw new BadCredentialsException("Authorization header is not Bearer type");
+        if (header == null || !header.startsWith("Bearer ")) {
+            LOG.info("No Authorization header or not Bearer authentication; can't log in");
+            // "anonymous" login -- AnonymousAuthenticationToken doesn't work because it doesn't allow null principals
+            //  or authorities, even though an anonymous user has no authentication principle and no authority.
+            return new UsernamePasswordAuthenticationToken(null, null, null);
         }
         String token = header.substring("Bearer ".length()).trim();
         LOG.info("Request token: '{}'", token);
